7079d6a10d7f1a218900f366d95b247bcebf665f
Marisol Acuña <mari.maf@gmail.com>
2015-10-28 08:35:43 -0300

    initial commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,48 @@
+class ItemsController < ApplicationController
+  before_filter :set_item, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @items = Item.all
+    respond_with(@items)
+  end
+
+  def welcome
+    @items = Item.all
+    respond_with(@items)
+  end
+
+  def show
+    respond_with(@item)
+  end
+
+  def new
+    @item = Item.new
+    respond_with(@item)
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(params[:item])
+    @item.save
+    respond_with(@item)
+  end
+
+  def update
+    @item.update_attributes(params[:item])
+    respond_with(@item)
+  end
+
+  def destroy
+    @item.destroy
+    respond_with(@item)
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Athena2::Application.config.secret_token = 'd96f1ec9507cf959aa6d79eafd5e17f125a58fef594d0833e6912ac72ed84845d32c98acbbcd88bd6d7056fcb69ecc2b3789042803f16a61bd8744f7f3277510'
